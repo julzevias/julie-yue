356a7f29263114ed1d8cada8d619632f24f5deef
Julie Yue <julie.yue.2011@gmail.com>
2021-08-29 21:30:24 -0500

    links to projects

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages';
-import SigninPage from './pages/Signin';
- 
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages";
+
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Switch>
-        <Route path={process.env.PUBLIC_URL + '/'} component={Home} exact />
-        <Route path={process.env.PUBLIC_URL + '/Signin'} component={SigninPage} exact />
- 
+        <Route path="/" component={Home} exact />
       </Switch>
-      
     </Router>
-    
   );
 }
- 
+
 export default App;
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,33 +1,64 @@
-import React from 'react';
-import Icon1 from '../../images/task.png';
-import Icon2 from '../../images/weather.png';
-import Icon3 from '../../images/question.png';
-import { ProjectsContainer, ProjectsH1, ProjectsWrapper, ProjectsCard, ProjectsIcon, ProjectsH2, ProjectsP } from './ProjectsElements'
- 
+import React from "react";
+import Icon1 from "../../images/task.png";
+import Icon2 from "../../images/weather.png";
+import Icon3 from "../../images/question.png";
+import {
+  ProjectsContainer,
+  ProjectsH1,
+  ProjectsWrapper,
+  ProjectsCard,
+  ProjectsIcon,
+  ProjectsH2,
+  ProjectsP,
+  Projectdiv,
+} from "./ProjectsElements";
+
 const Projects = () => {
-    return (
-        <ProjectsContainer id="projects">
-            <ProjectsH1>Projects</ProjectsH1>
-            <ProjectsWrapper>
-                <ProjectsCard>
-                    <ProjectsIcon src={Icon2}/>
-                    <ProjectsH2>Weather Application</ProjectsH2>
-                    <ProjectsP>Search a location for the weather and receive a cute background to boot!</ProjectsP>
-                </ProjectsCard>
-                <ProjectsCard>
-                    <ProjectsIcon src={Icon1}/>
-                    <ProjectsH2>Task Management</ProjectsH2>
-                    <ProjectsP>Keep track of your to-do's on this trusty productivity application!</ProjectsP>
-                </ProjectsCard>
-                <ProjectsCard>
-                    <ProjectsIcon src={Icon3}/>
-                    <ProjectsH2>Coming Soon</ProjectsH2>
-                     <ProjectsP>Keep Your Eyes Peeled!</ProjectsP> 
-                </ProjectsCard>
-            </ProjectsWrapper>
-            
-        </ProjectsContainer>
-    );
+  return (
+    <ProjectsContainer id="projects">
+      <ProjectsH1>Projects</ProjectsH1>
+      <ProjectsWrapper>
+        <ProjectsCard
+          href="https://www.instagram.com/julzevias/"
+          target="_blank"
+        >
+          <Projectdiv>
+            <ProjectsIcon src={Icon2} />
+            <ProjectsH2>Weather Application</ProjectsH2>
+            <ProjectsP>
+              Search a location for the weather and receive a cute background to
+              boot!
+            </ProjectsP>
+          </Projectdiv>
+        </ProjectsCard>
+
+        <ProjectsCard
+          href="https://www.instagram.com/julzevias/"
+          target="_blank"
+        >
+          <Projectdiv>
+            <ProjectsIcon src={Icon1} />
+            <ProjectsH2>Task Management</ProjectsH2>
+            <ProjectsP>
+              Keep track of your to-do's on this trusty productivity
+              application!
+            </ProjectsP>
+          </Projectdiv>
+        </ProjectsCard>
+
+        <ProjectsCard
+          href="https://www.instagram.com/julzevias/"
+          target="_blank"
+        >
+          <Projectdiv>
+            <ProjectsIcon src={Icon3} />
+            <ProjectsH2>Coming Soon</ProjectsH2>
+            <ProjectsP>Keep Your Eyes Peeled!</ProjectsP>
+          </Projectdiv>
+        </ProjectsCard>
+      </ProjectsWrapper>
+    </ProjectsContainer>
+  );
 };
- 
+
 export default Projects;
